a1c3116212eb37afe9a4abc63ff78025f31c39ee
keshi <keshi.wang@earlydata.com>
2018-09-26 15:34:42 +0800

    update

--- src/test/java/mytest/test.java
+++ src/test/java/mytest/test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(new Date());
         System.out.println("2018年9月26日15:22:09");
-                System.out.println("在GitHub上修改的");
-
+        System.out.println("在GitHub上修改的");
+        System.out.println("在idea修改的 2018年9月26日15:34:16");
     }
 }
