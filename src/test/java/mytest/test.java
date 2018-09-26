1b8e43adc9337180d8567f7e3d2e9ea0e9c18680
wkssmile <wkssmile@163.com>
2018-09-26 07:32:51 +0000

    Update test.java
    update

--- src/test/java/mytest/test.java
+++ src/test/java/mytest/test.java
@@ -6,5 +6,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(new Date());
         System.out.println("2018年9月26日15:22:09");
+                System.out.println("在GitHub上修改的");
+
     }
 }
