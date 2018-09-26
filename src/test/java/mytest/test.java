7fcc753769bcb09a59e069242562b2ba98d87dbe
keshi <keshi.wang@earlydata.com>
2018-09-26 15:26:10 +0800

    第二次提交

---
+++ src/test/java/mytest/test.java
@@ -0,0 +1,10 @@
+package mytest;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println("2018年9月26日15:22:09");
+    }
+}
